1550f368cd2de6bba9158f44c4bd174207a503a5
AciernoSimone <acierno.simone@iisgalvanimi.edu.it>
2020-05-11 09:58:22 +0000

    finito

--- index.js
+++ index.js
@@ -1,44 +1,30 @@
+
 const express = require('express');
 const app = new express();
-const sql = require('mssql');
-const cors = require('cors');
-const CC = require('./CoordConverter.js');
 
-app.use(new cors());
-
-const coordConverter =  new CC();
-
-const config = {
-    user: 'PCTO',  //Vostro user name
-    password: 'xxx123#', //Vostra password
-    server: "213.140.22.237",  //Stringa di connessione
-    database: 'Katmai', //(Nome del DB)
-}
+//Importo la classe per le chiamate al DB
+const sqlUtils = require('./SqlUtils.js'); 
 
 app.get('/', function (req, res) {
-    //res.send('Hello World!');
-    sql.connect(config, (err) => {
-        if (err) console.log(err);  // ... error check
-        else makeSqlRequest(res);     // 
-    });
+   //Per connettermi al DB uso il metodo statico sqlUtils.connect
+   //Passo come parametro la funzione sqlUtils.makeSqlRequest che verrà lanciata 
+   //se la connessione al DB avrà successo  
+   sqlUtils.connect(res, sqlUtils.makeSqlRequest);
 });
-
-function makeSqlRequest(res) {
-    let sqlRequest = new sql.Request();  //sqlRequest: oggetto che serve a eseguire le query
-    let q = 'SELECT DISTINCT TOP (100) [GEOM].STAsText() FROM [Katmai].[dbo].[interventiMilano]';
-    //eseguo la query e aspetto il risultato nella callback
-    sqlRequest.query(q, (err, result) => {sendQueryResults(err,result,res)}); 
-}
-
-
-function sendQueryResults(err,result, res)
-{
-    if (err) console.log(err); // ... error checks
-    res.send(coordConverter.generateGeoJson(result.recordset));  //Invio il risultato al Browser
-}
-
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+
+ //passo il foglio come parametro nell’url
+app.get('/ci_vettore/:foglio', function (req, res) {
+    console.log(req.params.foglio);
+    //richiamo il metodo che ottiene l'elenco dei vettori energetici
+    sqlUtils.connect(req, res, sqlUtils.ciVettRequest);
+ });
+
+
+
+
 
